708b479997cc7d64bdebe68545ca5003a13f4fb4
Paavan Kumar <penromenro@gmail.com>
2025-02-03 14:22:03 -0500

    Polished code

--- State.java
+++ State.java
@@ -6,18 +6,20 @@ public abstract class State {
         return name;
     }
 
-    public void setTax(SalesTaxBehavior tax) {
-        this.tax = tax;
-    }
-
-    public SalesTaxBehavior getTax() {
-        return tax;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
+    public void setTax(SalesTaxBehavior tax) { this.tax = tax; }
+
+    public SalesTaxBehavior getTax() { return tax; }
+
+    /**
+     * Prints out the tax of the value inputted to the console for the State
+     * @param value amount of money being taxed
+     */
     public void showTax(double value) {
+        String output = String.format("The sales tax on $%.02f in %s is $%.02f", value, name, tax.compute(value));
+        System.out.println(output);
     }
 }
